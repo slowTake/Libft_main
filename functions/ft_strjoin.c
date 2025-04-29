f4608a95b6d5ec205dc81efbea92cec76d6e712e
pnurmi <pnurmi.zt@gmail.com>
2025-04-29 13:58:05 +0300

    strjoin and substr done

---
+++ functions/ft_strjoin.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnurmi <pnurmi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 12:43:24 by pnurmi            #+#    #+#             */
+/*   Updated: 2025/04/29 13:57:42 by pnurmi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*newstr;
+	size_t	tlen;
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	i = 0;
+	tlen = ft_strlen(s1) + ft_strlen(s2);
+	newstr = (char *)malloc(tlen + 1);
+	if (newstr = NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		newstr[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		newstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	newstr[i] = '\0';
+	return (newstr);
+}
+// cats 2 strings into new string
+// requires memmory allocation
+// s1 and s2 prefix and suffix
+// returns new string or null if mem alloc fails
--- functions/ft_substr.c
+++ functions/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: pnurmi <pnurmi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 14:03:32 by pnurmi            #+#    #+#             */
-/*   Updated: 2025/04/28 15:34:09 by pnurmi           ###   ########.fr       */
+/*   Updated: 2025/04/29 13:57:16 by pnurmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,21 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	i = 0;
 	if (!s)
 		return (NULL);
-	else
-		substring = (char *)malloc(ft_strlen(s));
+	substring = (char *)malloc(ft_strlen(s));
 	if (substring == NULL)
 		return (NULL);
 	if (start > ft_strlen(s))
 	{
-		return (NULL);
+		return (ft_strdup(""));
 	}
-	while (s[i] < (start + len))
+	while (i < ft_strlen(s) && start < len)
 	{
-		if ((start + len) > ft_strlen(s))
-		{
-			substring[i++] = s[start++];
-		}
-		return (substring);
+		substring[i++] = s[start++];
 	}
+	substring[i] == '\0';
+	return (substring);
 }
 // start has to be withing the length of the given string (s)
 // start + len < full length
 // if start is bigger than len then return (strdup(""))
+// returns blank string if start is longer than *s
